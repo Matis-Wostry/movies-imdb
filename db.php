293181bf1a55cec835abfca5fc1b392467939bdf
Matis <matiswostry@gmail.com>
2026-04-18 12:58:58 +0200

    feat: add CI/CD

--- db.php
+++ db.php
@@ -1,7 +1,12 @@
 <?php
+$host = getenv('DB_HOST') ?: 'localhost';
+$name = getenv('DB_NAME') ?: 'movies_imdb';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASSWORD') ?: 'root';
+
 $pdo = new PDO(
-    'mysql:host=localhost;dbname=movies_imdb;charset=utf8mb4',
-    'root',
-    'root',
+    "mysql:host=$host;dbname=$name;charset=utf8mb4",
+    $user,
+    $pass,
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 );
